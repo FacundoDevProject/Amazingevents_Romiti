5e80986034084c83b349c35236605f200e56524b
Facu <argentinaprogramap2fr@gmail.com>
2023-02-26 22:40:31 -0300

    commit 1
    task1

---
+++ assets/js/showData.js
@@ -0,0 +1,28 @@
+/* console.log("Hola mundo!");
+
+let nombre = "Facundo";
+
+console.log(nombre)
+
+let persona = {
+    nombre: "Rafael",
+    edad: 25,
+}
+
+console.log(persona)
+console.log(persona.nombre)
+
+let frutas = ["Pera", "Sandia"]
+
+console.log(frutas)
+console.log(frutas[1])
+console.log(frutas[4]) */
+
+// esto es un comentario //
+
+
+
+/* let nombreIngresado = prompt("Ingrese su nombre: ")
+console.log(nombreIngresado)
+
+alert("Hola, bienvenido " + nombreIngresado) */
