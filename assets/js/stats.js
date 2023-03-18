f81d72ce764676d7d0d3f57a4d0347773cc2999e
Facu <argentinaprogramap2fr@gmail.com>
2023-03-18 00:11:48 -0300

    tablas sin estilo

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -2,89 +2,122 @@ let categories = [];
 
 let eventsarray = [];
 
+let currentDate;
+
+let table = document.getElementById("tbody1");
+
 async function getData() {
     try {
         let apiUrl = '../assets/js/amazingevents.json'
         let response = await fetch(apiUrl);
         let data = await response.json();
-        console.log(data);
         eventsarray = data.events;
+        currentDate = data.currentDate;
         categories = createCategories(eventsarray);
+        let maxAttendanceEvent = eventsarray.sort((a, b) => ((b.assistance) / (b.capacity)) - ((a.assistance) / (a.capacity)))[0];
+        let minAttendanceEvent = eventsarray.sort((a, b) => ((a.assistance) / (a.capacity)) - ((b.assistance) / (b.capacity)))[0];
+        let capacityEvent = maxCapacityEvent(eventsarray);
+        addFile(maxAttendanceEvent.name, minAttendanceEvent.name, capacityEvent.name, table);
+        createCategoryTable(eventsarray, categories, currentDate)
+        createCategoryTable2(eventsarray, categories, currentDate)
+
     }
     catch (error) {
         console.log(error);
     }
 }
 getData()
-/* Necesario para que funcion Get Data */
+/* Necesario para que funcione Get Data */
 const createCategories = (data) => {
     let categories = data.map(category => category.category)
     let categoriesUnrepeat = [...(new Set(categories))]
     return categoriesUnrepeat
 }
 
-/* PRIMERA TABLA
-EN PRIMER COLUMNA: EVENTO CON MAYOR PORCENTAJE DE ASISTENCIA DEL ARRAY DE TODOS LOS EVENTOS.
-SORT() Y QUE ENTREGUE EL PRIMER VALOR (ORDENANDO DE MAYOR A MENOR)
+/* FUNCION DE ORDENAMIENTO */
 
-SEGUNDA COLUMNA: EVENTO CON MENOR PORCENTAJE DE ASISTENCIA DEL ARRAY DE TODOS LOS EVENTOS
-SORT() Y QUE ENTREGUE EL PRIMER VALOR (ORDENANDO DE MENOR A MAYOR)
+function maxCapacityEvent(events) {
+    let capacityEvent = events.sort((a, b) => b.capacity - a.capacity);
+    return capacityEvent[0];
+}
 
-TERCER COLUMNA: EVENTO CON MAYOR CAPACIDAD DE TODOS.
-SORT() A CAPACITY Y MISMA FUNCION QUE COLUMNA 1
-*/
+/*AGREGAR FILA */
 
-/* SEGUNDA Y TERCER TABLA:
-CON LOS ARRAY DE UPCOMING EVENTS Y PAST EVENTS RESPECTIVAMENTE, O CON EL ARRAY TOTAL FILTRADO POR FECHA ">" "<"
-REALIZAR
-PRIMER COLUMNA: COLOCAR UN TODAS LAS CATEGORIAS
-SEGUNDA COLUMNA: USAR UN ACUMULADOR PARA SUMAR LAS GANANCIAS DE EVENTOS POR CATEGORIA
-TERCER COLUMNA: MISMO QUE DOS PERO CON ASISTENCIA */
+function addFile(para1, para2, para3, container) {
+    let newRow = document.createElement("tr");
+    let celdaColA = document.createElement("td");
+    let celdaColB = document.createElement("td");
+    let celdaColC = document.createElement("td");
 
-/* function crearTable() {
-    let body = document.getElementsByTagName("body")[0];
-    let table = document.createElement("table");
-    body.appendChild(table);
+    celdaColA.textContent = `${para1}`;
+    celdaColB.textContent = `${para2}`;
+    celdaColC.textContent = `${para3}`;
 
-    let tblBody = document.createElement("tbody");
-    table.appendChild(tblBody);
+    newRow.appendChild(celdaColA);
+    newRow.appendChild(celdaColB);
+    newRow.appendChild(celdaColC);
+    container.appendChild(newRow);
+}
 
-    for (let i = 0; i < 3; i++) {
-        let hilera = document.createElement("tr");
-        for (let j = 0; j < 3; j++) {
-            let celda = document.createElement("td");
-            let textoCelda = document.createTextNode("Texto a completar" + j);
-            celda.appendChild(textoCelda);
-            hilera.appendChild(celda);
+
+/* TABLA NUMERO 2 */
+
+function createCategoryTable(eventsarray, categories, currentDate) {
+    let secondTable = document.getElementById("tbody2");
+    categories.forEach(category => {
+        let filteredEvents = eventsarray.filter((event) => event.category === category).filter((event) => event.date < currentDate)
+        let eventsRevenueEstimate = 0;
+        let eventPerCategory = 0;
+        for (let i = 0; i < filteredEvents.length; i++) {
+            eventsRevenueEstimate += filteredEvents[i].price * filteredEvents[i].assistance;
+            eventPerCategory+= 1;
         }
-        tblBody.appendChild(hilera);
-    }
-    let cell1 = document.createElement("td");
-    let maxAttendanceEvent = eventsarray.sort((a, b) => ((b.assistance) / (b.capacity)) - ((a.assistance) / (a.capacity))[0]);
-    let maxAttendanceText = document.createTextNode(maxAttendanceEvent.name);
-    cell1.appendChild(maxAttendanceText);
-    hilera.appendChild(cell1);
-    table.setAttribute("border", "2");
-}
-
-crearTable(); */
-
-function crearTable() {
-    let body = document.getElementsByTagName("body")[0];
-    let table = document.createElement("table");
-    body.appendChild(table);
-
-    let tblBody = document.createElement("tbody");
-    table.appendChild(tblBody);
-
-    let row1 = document.createElement("tr");
-    let cell1 = document.createElement("td");
-    let maxAttendanceEvent = eventsarray.sort((a, b) => ((b.assistance) / (b.capacity)) - ((a.assistance) / (a.capacity)))[0];
-    let maxAttendanceText = document.createTextNode(maxAttendanceEvent.name);
-    cell1.appendChild(maxAttendanceText);
-    row1.appendChild(cell1);
-
+        let eventsPercentageAtt = 0;
+        filteredEvents.forEach(event => {
+            let eventAtt = ((event.assistance*100)/event.capacity)/eventPerCategory;
+            eventsPercentageAtt += eventAtt;
+        }
+        )
+        let newObject = {
+            name: category,
+            quantity: eventPerCategory,
+            revenue: `$${eventsRevenueEstimate}`,
+            attendance: `${eventsPercentageAtt.toFixed(2)}%`,  
+        }
+        addFile(newObject.name, newObject.revenue, newObject.attendance, secondTable);
+    })
 
 }
+
+/*TERCER TABLA */
+
+function createCategoryTable2(eventsarray, categories, currentDate) {
+    let thirdTable = document.getElementById("tbody3");
+    categories.forEach(category => {
+        let filteredEvents = eventsarray.filter((event) => event.category === category).filter((event) => event.date > currentDate)
+        let eventsRevenueEstimate = 0;
+        let eventPerCategory = 0;
+        for (let i = 0; i < filteredEvents.length; i++) {
+            eventsRevenueEstimate += filteredEvents[i].price * filteredEvents[i].estimate;
+            eventPerCategory+= 1;
+        }
+        let eventsPercentageAtt = 0;
+        filteredEvents.forEach(event => {
+            let eventAtt = ((event.estimate*100)/event.capacity)/eventPerCategory;
+            eventsPercentageAtt += eventAtt;
+        }
+        )
+        let newObject = {
+            name: category,
+            quantity: eventPerCategory,
+            revenue: `$${eventsRevenueEstimate}`,
+            attendance: `${eventsPercentageAtt.toFixed(2)}%`,  
+        }
+        addFile(newObject.name, newObject.revenue, newObject.attendance, thirdTable);
+    })
+}
+
+
+
 
 
